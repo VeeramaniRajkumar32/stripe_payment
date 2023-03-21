cf56e8e0dfbfee96257e9b60b6ac5556660f33a5
veeramanirajkumar32 <veeramanirajkumar32@gmail.com>
2023-03-21 18:28:04 +0530

    payment card frontend work

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 
 function App() {
+  const [product, setProduct] = useState({
+    name: "Veera Product",
+    price: 5000 * 100,
+    productBy: "veeramani",
+  });
+
+  const makePayment = (token) => {
+	const body = {
+		token,
+		product	
+	}
+	
+	const headers = {
+		"Content-Type" : "application/json",
+	}
+	return fetch("http://localhost:8000/payments",
+	{
+		method: "POST",
+		headers,
+		body: JSON.stringify(body),
+	})
+	.then((response) => {
+		console.log(response);
+	})
+	.catch((err) => {
+		console.log(err);
+	})
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <StripeCheckout
+        name={product.name}
+        amount={product.price}
+        currency="INR"
+		token={makePayment}
+        stripeKey="pk_test_51MnxckSBqeTFR9qwK1YmNAvIGgSwm7JA9Gaz02oPM17MoSgDNaZ9heOk5Qyk3EL45H4K6I14OJO3h8wIi4pDRs4N00PEIG9mml"
+      >
+        <button>Pay to {product.price / 100}</button>
+      </StripeCheckout>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 
-
+app.use(express.json())
+app.use('/payments', (req, res)=>{
+    console.log(req.body);
+    // console.log(res);
+})
 
 app.listen(8000, () => {
     console.log(`server listen on the ${8000}`);
