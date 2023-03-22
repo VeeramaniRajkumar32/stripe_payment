622743429e427022c3937a6c192b609937d891b0
veeramanirajkumar32 <veeramanirajkumar32@gmail.com>
2023-03-22 17:27:48 +0530

    completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import StripeCheckout from "react-stripe-checkout";
+// import axios from "axios";
 
 function App() {
   const [product, setProduct] = useState({
@@ -9,39 +10,55 @@ function App() {
   });
 
   const makePayment = (token) => {
-	const body = {
-		token,
-		product	
-	}
-	
-	const headers = {
-		"Content-Type" : "application/json",
-	}
-	return fetch("http://localhost:8000/payments",
-	{
-		method: "POST",
-		headers,
-		body: JSON.stringify(body),
-	})
-	.then((response) => {
-		console.log(response);
-	})
-	.catch((err) => {
-		console.log(err);
-	})
-  }
+    const body = {
+      token,
+      product,
+    };
+    console.log(body);
+
+    // axios.post('/payments', body)
+    //                 .then((response) => {
+    //                   console.log(response);
+    //                   return response
+    //                 }).catch((err) => {
+    //                   console.log(err);
+    //                 })
+    
+
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    return fetch("http://localhost:8000/payments", {
+      method: "POST",
+      headers,
+      body: JSON.stringify(body),
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
-    <div>
-      <StripeCheckout
+    <div><stripe-buy-button
+    buy-button-id="buy_btn_1MoPwmSBqeTFR9qw1xH9Xk5l"
+    // publishable-key="pk_live_51MnxckSBqeTFR9qwv9o43C1pQZstuPCGNeG1IUTndtSIMWg19LvKtXlRY691PNl6cG0mgEzAx5sOvea1XmRQB0Hz00Bh6AI9Cl"
+      // <StripeCheckout
         name={product.name}
-        amount={product.price}
+        amount={product.price }
         currency="INR"
-		token={makePayment}
+        token={makePayment}
         stripeKey="pk_test_51MnxckSBqeTFR9qwK1YmNAvIGgSwm7JA9Gaz02oPM17MoSgDNaZ9heOk5Qyk3EL45H4K6I14OJO3h8wIi4pDRs4N00PEIG9mml"
+        // stripeKey="pk_live_51MnxckSBqeTFR9qwv9o43C1pQZstuPCGNeG1IUTndtSIMWg19LvKtXlRY691PNl6cG0mgEzAx5sOvea1XmRQB0Hz00Bh6AI9Cl"
       >
         <button>Pay to {product.price / 100}</button>
-      </StripeCheckout>
+  {/* > */}
+  </stripe-buy-button>
+      {/* </StripeCheckout> */}
     </div>
+    
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -1,11 +1,52 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const {v4:uuid} = require('uuid')
+const stripe  = require('stripe')('sk_test_51MnxckSBqeTFR9qwmG2aC3NtF5LXbDENTzxtqMGUXW4scQQoJp07DLNsGeeZZEyuKwapd1XFHPP6AhxFJx6WVjtE00aYoKeqSO')
+// const stripe  = require('stripe')('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')
 
+app. use(cors())
 app.use(express.json())
-app.use('/payments', (req, res)=>{
-    console.log(req.body);
-    // console.log(res);
-})
+app.use('/payments', async (req, res)=>{
+    const { product, token} = req.body
+    const transactionKey = uuid();
+    console.log(product);
+    console.log(token);
+   return stripe.customers.create({
+        email: token.email,
+        source: token.id    
+    })
+    // .then((customer) => {
+    //     customer.charges.create({
+    //     amount: product.price,
+    //     currency: "inr",
+    //     customer: customer.id,
+    //     receipt_email: token.email,
+    //     description: product.name
+    // })
+        .then((result) => {
+            res.json(result)
+        }).catch((err) => {
+            console.log(err);
+        })
+    })
+        // try {
+        //     const customer = await stripe.customers.create({
+        //         email : token.email,
+        //         source : token.id
+        //     })
+        //     const charges = await stripe.charges.create({
+        //         amount : product.price,
+        //         currency: 'inr',
+        //         customer: customer.id,
+        //         receipt_email: token.email,
+        //         description: product.name
+        //     })
+        //     return res.json(charges)
+        // } catch (error) {
+        //     console.log(error);
+        // }
+// })
 
 app.listen(8000, () => {
     console.log(`server listen on the ${8000}`);
